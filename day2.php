0d3b3f43326fe2f74f0cbde71611649727be9960
Reignnnn <dagumreigngaebriell@gmail.com>
2025-10-31 23:38:29 +0800

    day2.php

---
+++ day2.php
@@ -0,0 +1,23 @@
+<?php 
+
+$a = 10;
+$b = 20;
+$c = null;
+
+
+//addition
+$c = $a + $b;
+//multiplication
+$c = $a * $b;
+//subtraction
+$c = $a - $b;
+//division
+$c = $a / $b;
+//modulus
+$c = $a % $b;
+//exponentiation
+$c = $a ** $b;
+
+echo $c;
+
+?>  
