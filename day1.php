c0ad3a9f87164e52cc329068ce110bcd45fc7520
Reignnnn <dagumreigngaebriell@gmail.com>
2025-10-31 23:36:43 +0800

    day01.php

---
+++ day1.php
@@ -0,0 +1,13 @@
+<?php
+$name="Reign";
+$age=19;
+$food="burger";
+$school="QCU";
+
+echo"My name is {$name} <br>";
+echo"I am {$age} years old <br>";
+echo"My favorite food is {$food} <br>";
+echo"I study at {$school} <br>";
+
+
+?>
